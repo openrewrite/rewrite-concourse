9fe3ac116cd827e97a240dd36260874571efa8e4
Jonathan Schneider <jkschneider@gmail.com>
2021-09-08 10:43:47 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
+### Migrate Concourse. Automatically.
+
+[![ci](https://github.com/openrewrite/rewrite-concourse/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-concourse/actions/workflows/ci.yml)
+[![Apache 2.0](https://img.shields.io/github/license/openrewrite/rewrite-concourse.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-concourse.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-concourse)
+
+### What is this?
+
+This project implements [Rewrite recipes](https://github.com/openrewrite/rewrite) to perform Concourse hygiene and migration tasks.
---
+++ src/main/java/org/openrewrite/concourse/FindResource.java
@@ -0,0 +1,46 @@
+package org.openrewrite.concourse;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.Tree;
+import org.openrewrite.yaml.JsonPathMatcher;
+import org.openrewrite.yaml.YamlVisitor;
+import org.openrewrite.yaml.search.YamlSearchResult;
+import org.openrewrite.yaml.tree.Yaml;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class FindResource extends Recipe {
+    @Option(displayName = "Type",
+        description = "Resource type",
+        example = "git")
+    String type;
+
+    @Override
+    public String getDisplayName() {
+        return "Find resource";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Find a Concourse resource by name";
+    }
+
+    @Override
+    protected YamlVisitor<ExecutionContext> getSingleSourceApplicableTest() {
+        JsonPathMatcher resource = new JsonPathMatcher("$.resources[?(@.type == '" + type + "')]");
+
+        return new YamlVisitor<ExecutionContext>() {
+            @Override
+            public Yaml visitMappingEntry(Yaml.Mapping.Entry entry, ExecutionContext executionContext) {
+                if(resource.matches(getCursor())) {
+                    return entry.withMarkers(entry.getMarkers().addIfAbsent(new YamlSearchResult(Tree.randomId(), FindResource.this, null)));
+                }
+                return super.visitMappingEntry(entry, executionContext);
+            }
+        };
+    }
+}
